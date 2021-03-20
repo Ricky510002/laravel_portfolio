684ad522b5ffff179cc05b381c1f7ac0c41ba6a6
Ryuki Goto <rth510ryu@gmail.com>
2021-03-20 14:28:49 +0900

    タイトルの変更

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>BookMatch</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -23,7 +23,7 @@
 
     <!-- ファビコン -->
     <link rel="bookmatch icon" href="{{ asset('bookmatch.ico') }}">
-    
+
 </head>
 <body>
     <div id="app">
